96c2f6d85e22f7fb6bd69e54ae6b21e4f14918c7
nuncamaria <nunezcanovas@gmail.com>
2024-09-12 13:21:14 +0200

    add permission launcher

--- app/src/main/java/com/nuncamaria/learningbluetooth/ui/MainActivity.kt
+++ app/src/main/java/com/nuncamaria/learningbluetooth/ui/MainActivity.kt
@@ -1,19 +1,56 @@
 package com.nuncamaria.learningbluetooth.ui
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import com.nuncamaria.learningbluetooth.ui.theme.LearningBluetoothTheme
 
 class MainActivity : ComponentActivity() {
+
+    // This has to be in both, BluetoothServerImpl and here in the Activity
+    private val bluetoothManager by lazy { applicationContext.getSystemService(BluetoothManager::class.java) }
+    private val bluetoothAdapter by lazy { bluetoothManager?.adapter }
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            LearningBluetoothTheme {
-                LearningBtView()
+
+        val enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {}
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permission ->
+            val canEnableBluetooth =
+                permission[android.Manifest.permission.BLUETOOTH_CONNECT] == true
+
+            if (canEnableBluetooth && !isBluetoothEnabled) {
+                enableBluetoothLauncher.launch(
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                )
             }
+        }
+
+        permissionLauncher.launch(
+            arrayOf(
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            )
+        )
+
+        setContent {
+            LearningBluetoothTheme { MainView() }
         }
     }
 }
