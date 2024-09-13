1f4e3550f224f408128a38c8dc0638600668aff8
nuncamaria <nunezcanovas@gmail.com>
2024-09-13 15:31:54 +0200

    Add service uuid

---
+++ app/src/main/java/com/nuncamaria/learningbluetooth/domain/Constants.kt
@@ -0,0 +1,10 @@
+package com.nuncamaria.learningbluetooth.domain
+
+import java.util.UUID
+
+/**
+ * UUID for the service, should be the same for both devices, so it can not be randomly created
+ * And it has to be in the format of a UUID
+ * https://www.uuidgenerator.net/version4
+ */
+val SERVICE_UUID: UUID = UUID.fromString("1ab1711a-2e34-4b74-911c-6f99379e6787")
