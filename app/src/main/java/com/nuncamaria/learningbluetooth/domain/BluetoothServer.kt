e039a8ee2043183e37f50b320fec59140d916349
nuncamaria <nunezcanovas@gmail.com>
2024-09-17 12:40:30 +0200

    implement registerReceiver

--- app/src/main/java/com/nuncamaria/learningbluetooth/domain/BluetoothServer.kt
+++ app/src/main/java/com/nuncamaria/learningbluetooth/domain/BluetoothServer.kt
@@ -2,12 +2,16 @@ package com.nuncamaria.learningbluetooth.domain
 
 import android.bluetooth.BluetoothDevice
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 
 interface BluetoothServer {
+    val isDeviceConnected: StateFlow<Boolean>
 
     val scannedDevices: StateFlow<List<BluetoothDevice>>
     val pairedDevices: StateFlow<List<BluetoothDevice>>
+
+    val errors: SharedFlow<String>
 
     // Start and stop looking for devices
     fun startServer()
--- app/src/main/java/com/nuncamaria/learningbluetooth/domain/BluetoothServerImpl.kt
+++ app/src/main/java/com/nuncamaria/learningbluetooth/domain/BluetoothServerImpl.kt
@@ -1,6 +1,7 @@
 package com.nuncamaria.learningbluetooth.domain
 
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothServerSocket
@@ -9,14 +10,20 @@ import android.content.Context
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import com.nuncamaria.learningbluetooth.domain.brodcastreceiver.FoundDeviceReceiver
+import com.nuncamaria.learningbluetooth.domain.brodcastreceiver.StateDeviceReceiver
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 @SuppressLint("MissingPermission")
@@ -35,15 +42,31 @@ class BluetoothServerImpl(private val ctx: Context) : BluetoothServer {
     private val isBluetoothEnabled: Boolean
         get() = bluetoothAdapter?.isEnabled == true
 
+    private val _isDeviceConnected = MutableStateFlow(false)
+    override val isDeviceConnected = _isDeviceConnected.asStateFlow()
+
     private val _scannedDevices = MutableStateFlow<List<BluetoothDevice>>(emptyList())
     override val scannedDevices = _scannedDevices.asStateFlow()
 
     private val _pairedDevices = MutableStateFlow<List<BluetoothDevice>>(emptyList())
     override val pairedDevices = _pairedDevices.asStateFlow()
 
+    private val _errors = MutableSharedFlow<String>()
+    override val errors: SharedFlow<String> = _errors.asSharedFlow()
+
     private val foundDeviceReceiver = FoundDeviceReceiver { device ->
         _scannedDevices.update { devices ->
             if (device in devices) devices else devices
+        }
+    }
+
+    private val stateDeviceReceiver = StateDeviceReceiver { isConnected, device ->
+        if (bluetoothAdapter?.bondedDevices?.contains(device) == true) { // check if the device we're trying to connect is in our bonded devices
+            _isDeviceConnected.update { isConnected }
+        } else {
+            CoroutineScope(Dispatchers.IO).launch {
+                _errors.emit("Can't connect to a non-paired device")
+            }
         }
     }
 
@@ -52,6 +75,14 @@ class BluetoothServerImpl(private val ctx: Context) : BluetoothServer {
 
     init {
         updatePairedDevices()
+        ctx.registerReceiver(
+            stateDeviceReceiver,
+            IntentFilter().apply {
+                addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED)
+                addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
+                addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
+            }
+        )
     }
 
     override fun startServer() {
@@ -139,7 +170,10 @@ class BluetoothServerImpl(private val ctx: Context) : BluetoothServer {
     }
 
     override fun unregisterReceiver() {
+        // we need to unregister receivers to avoid memory leaks
         ctx.unregisterReceiver(foundDeviceReceiver)
+        ctx.unregisterReceiver(stateDeviceReceiver)
+        closeConnection()
     }
 
     private fun updatePairedDevices() {
